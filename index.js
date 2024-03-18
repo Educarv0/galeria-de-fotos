e76541f55ad1aaf32a746dc87fa19956f257a94c
Educarv0 <160619368+Educarv0@users.noreply.github.com>
2024-03-18 19:34:12 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+
+$(document).ready(function() {
+    $('.fotos').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery:{
+            enabled:true
+        }
+    })
+    $('.videos').magnificPopup({
+        delegate: 'a',
+        type: 'iframe',
+        gallery:{
+            enabled:true
+        }
+    })
+});
+
+const animationElements = document.querySelectorAll('.animate-on-scroll');
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting){
+            entry.target.classList.add('animate');
+        } else {
+            entry.target.classList.remove('animate');
+        }
+    })
+}, {
+    threshold: 0.5
+});
+
+for (let i = 0; i < animationElements.length; i++){
+    const el = animationElements[i];
+    observer.observe(el);
+}
